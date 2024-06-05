0de5153fcf36283d57ab3bfa4e79dd67d38f99b1
jarethholt <jareth.holt@gmail.com>
2024-06-05 09:26:58 +0200

    Add project files.

---
+++ MoqExercise/Models/Reservation.cs
@@ -0,0 +1,9 @@
+﻿namespace MoqExercise.Models;
+
+public class Reservation
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string StartLocation { get; set; }
+    public string EndLocation { get; set; }
+}
