63f983f2cb29359bbb86829b610c1f6977708e5b
jarethholt <jareth.holt@gmail.com>
2024-06-05 11:19:52 +0200

    feat: Add ReservationsController

---
+++ MoqExercise/Controllers/ReservationsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoqExercise.Models;
+using MoqExercise.Repositories;
+
+namespace MoqExercise.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ReservationsController(IReservationRepository repo) : ControllerBase
+{
+    private readonly IReservationRepository _repo = repo;
+
+    [HttpGet]
+    public IEnumerable<Reservation> Get() =>
+        _repo.Reservations;
+
+    [HttpGet("{id}")]
+    public ActionResult<Reservation> Get(int id)
+    {
+        if (id == 0)
+            return BadRequest("Value must be passed in the request body");
+
+        var reservation = _repo[id];
+        if (reservation is null)
+            return NotFound();
+        return Ok(reservation);
+    }
+
+    [HttpPost]
+    public Reservation Post([FromBody] Reservation res) =>
+        _repo.AddReservation(new Reservation
+        {
+            Name = res.Name,
+            StartLocation = res.StartLocation,
+            EndLocation = res.EndLocation
+        });
+
+    [HttpDelete("{id}")]
+    public void Delete(int id) =>
+        _repo.DeleteReservation(id);
+}
