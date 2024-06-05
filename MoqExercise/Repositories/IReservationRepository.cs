2f45e93c4631f01159e6dfa617a1e98f892d2245
jarethholt <jareth.holt@gmail.com>
2024-06-05 09:46:30 +0200

    feat: Add ReservationRepository and corresponding interface

---
+++ MoqExercise/Repositories/IReservationRepository.cs
@@ -0,0 +1,12 @@
+﻿using MoqExercise.Models;
+
+namespace MoqExercise.Repositories;
+
+public interface IReservationRepository
+{
+    IEnumerable<Reservation> Reservations { get; }
+    Reservation? this[int id] { get; }
+    Reservation AddReservation(Reservation reservation);
+    Reservation UpdateReservation(Reservation reservation);
+    void DeleteReservation(int id);
+}
---
+++ MoqExercise/Repositories/ReservationRepository.cs
@@ -0,0 +1,42 @@
+﻿using MoqExercise.Models;
+
+namespace MoqExercise.Repositories;
+
+public class ReservationRepository : IReservationRepository
+{
+    private Dictionary<int, Reservation> items = [];
+
+    public ReservationRepository()
+    {
+        new List<Reservation>()
+        {
+            new() { Id = 1, Name = "Ankit", StartLocation = "New York", EndLocation = "Beijing" },
+            new() { Id = 2, Name = "Bobby", StartLocation = "New Jersey", EndLocation = "Boston" },
+            new() { Id = 3, Name = "Clara", StartLocation = "London", EndLocation = "Paris" },
+        }.ForEach(r => AddReservation(r));
+    }
+
+    public Reservation? this[int id] =>
+        items.TryGetValue(id, out Reservation? value) ? value : null;
+
+    public IEnumerable<Reservation> Reservations =>
+        items.Values;
+
+    public Reservation AddReservation(Reservation reservation)
+    {
+        if (reservation.Id < 1)
+        {
+            int key = 1;
+            while (items.ContainsKey(key)) key++;
+            reservation.Id = key;
+        }
+        items[reservation.Id] = reservation;
+        return reservation;
+    }
+
+    public void DeleteReservation(int id) =>
+        items.Remove(id);
+
+    public Reservation UpdateReservation(Reservation reservation) =>
+        AddReservation(reservation);
+}
